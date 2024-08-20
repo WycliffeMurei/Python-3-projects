d8f5dc0830f1b8c36287d8a1eb33a87762df2143
WycliffeMurei <105030660+WycliffeMurei@users.noreply.github.com>
2024-08-20 09:18:44 +0300

    Add files via upload

---
+++ shopping list entry.py
@@ -0,0 +1,22 @@
+#variable  declaration
+
+
+
+
+#data types
+name=str(input("Enter your name:          "))
+product=str(input("Enter product:             "))
+quantity=int(input("Enter quantity:            "))
+price=int(input("Enter price:      "))
+unit_length= float(input("enter lenght of product"))
+
+
+
+
+
+#print variables
+print("your name is  ", name )
+print("your product is  ",product)
+print("the quantity of the product is  ",quantity)
+print("the price is  ",price)
+
